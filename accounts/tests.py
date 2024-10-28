1ff80a15bd527c6507b07d423e2e4f3d634366c2
asiqul islam <asiq.dev@gmail.com>
2024-10-29 01:32:52 +0600

    adding email field in registration form and write test case

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class SignUpPageTests(TestCase):
+    def test_url_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "monjur",
+                "email": "monjur@gmail.com",
+                "password1": "Admin123#",
+                "password2": "Admin123#",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "monjur")
+        self.assertEqual(get_user_model().objects.all()[0].email, "monjur@gmail.com")
