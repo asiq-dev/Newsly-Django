3326711f22d384872fd35deebdf9d2e81952f28d
asiqul islam <asiq.dev@gmail.com>
2024-10-30 17:28:09 +0600

    redesign login and logout page also added crispy form and bootstrap crispy in the project

--- daily_news/settings.py
+++ daily_news/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # local apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
@@ -127,3 +131,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
