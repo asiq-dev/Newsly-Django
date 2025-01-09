8a6a693bf0d2e1b31cc276bd6488851c359fd931
asiqul islam <asiq.dev@gmail.com>
2025-01-09 23:38:52 +0600

    home page design and data dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
+from articles.models import Article
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["latest_article"] = Article.objects.order_by('-date')[:3]
+        return context
